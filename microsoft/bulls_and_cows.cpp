5a3a096393efcf34b8077b0ff99b8ab4943d672f
unknown <aliabbas7317@gmail.com>
2023-01-02 23:41:49 +0530

    Third Problem -> bulls and cows, status solved

--- microsoft/bulls_and_cows.cpp
+++ microsoft/bulls_and_cows.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-        
+        int x=0;//number of bulls
+        unordered_map<char,int> hashCountChar;
+        for(int i=0;i<secret.length();i++){
+            if(secret[i]==guess[i]){
+                x++;//counting bulls
+                //modifying given value because they are passed by value so we have a copy of original strings here.
+                secret[i]='x';
+                guess[i]='y';
+            }
+            //hashing the guess string characters with frequency count.
+            if(hashCountChar.find(guess[i])==hashCountChar.end()) hashCountChar[guess[i]]=1;
+            else hashCountChar[guess[i]]++;
+        }
+        int y=0;//number of cows.
+        for(int i=0;i<guess.length();i++){
+            if(hashCountChar.find(secret[i])!=hashCountChar.end()){
+                if(hashCountChar[secret[i]]>0){
+                    y++;//counting cows
+                    hashCountChar[secret[i]]--;//removing the already counted cow from the map
+                }
+            }
+        }
+        return to_string(x)+"A"+to_string(y)+"B";
     }
 };
