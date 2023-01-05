b597b7166720d35090c59e3b010e3f587dacf051
unknown <aliabbas7317@gmail.com>
2023-01-05 18:05:46 +0530

    Fourteenth Problem -> minimum deletion to make array divisible, status solved

--- microsoft/minimum_deletions_to_make_array_divisible.cpp
+++ microsoft/minimum_deletions_to_make_array_divisible.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, vector<int>& numsDivide) {
-        
+        int ans = -1;
+        int n = nums.size();
+        sort(nums.begin() , nums.end());
+        int gcdd = *max_element(numsDivide.begin() , numsDivide.end());
+        for(auto i:numsDivide)
+        {
+            gcdd = __gcd(gcdd , i);
+        }
+        cout<<gcdd<<"\n";
+        for(int i=0;i<n;++i)
+        {
+            if(gcdd%nums[i] == 0)
+            {
+               ans = i;
+                break;
+            } 
+        }
+        return ans;
     }
 };
